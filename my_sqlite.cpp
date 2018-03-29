73022614a926766427569e46d250f5fe59c31f88
wangkeyi <wangkeyi203@163.com>
2018-03-30 01:06:57 +0800

    test my_sqlite fun()

--- my_sqlite.cpp
+++ my_sqlite.cpp
@@ -5,7 +5,6 @@ My_Sqlite::My_Sqlite(QString con_num)
 {
     db = QSqlDatabase::addDatabase("QSQLITE",con_num);
     db.setDatabaseName("DATABASE");
-    write_time = new QString;
     query = new QSqlQuery(con_num);
     if (db.open())
     {
@@ -20,46 +19,66 @@ My_Sqlite::My_Sqlite(QString con_num)
 
 bool My_Sqlite::test_add(QString test)
 {
-    QString id;
-    query->exec("select max(id) from test");
-    while(query->next())
+    QString test1 = "张三";
+    QString test2 = "黄花";
+    QString test3 = "10";
+    if(this->add_weight_1(test1,test2,test3) == false)
     {
-        id = "4";
-        qDebug() << query->value(0).toInt();
-    }
-    query->prepare("insert into test values (:test_id,:test,:test)");
-    query->bindValue(":test_id",id.toInt());
-    query->bindValue(":test",test);
-    query->exec();
-    if(!query->isActive())
-    {
-        qDebug() << "add test error";
-        return false;
-    }
-    else {
-        return true;
+        qDebug() << " add_weight_1 failed";
     }
 }
 bool My_Sqlite::add_weight_1(QString worker_name,QString kind,QString weight)
 {
     query->exec("select max(id) from list");
+    int id;
     while(query->next())
     {
-        int id = query->value(0).toInt()+1;
+        id = query->value(0).toInt()+1;
     }
     write_time = time.currentDateTime().toString("yyyy.MM.dd hh:mm:ss");
-    //再将id写入worker表中
-    query->prepare("update into worker valude (list_id=:id)");
+    //再将id写入worker表中,用来第二次写入的时候查询
+    query->prepare("update worker set list_id=:id where name=:worker_name");
     query->bindValue(":id",QString::number(id,10));
-
-
+    query->bindValue(":worker_name",worker_name);
+    query->exec();
+    if(!query->isActive())
+    {
+        return false;
+    }
     query->prepare("insert into list values (id=:list_id,name=:worker_name,kind=:list_kind,weight1=:list_weight1,time1=:list_time1)");
     query->bindValue(":list_id",QString::number(id,10));
     query->bindValue(":worker_name",worker_name);
     query->bindValue(":list_kind",kind);
     query->bindValue(":list_weight",weight);
     query->bindValue(":list_time1",write_time);
+    query->exec();
+    if(!query->isActive())
+    {
+        return false;
+    }
+    return true;
+}
 
+bool My_Sqlite::add_weight_2(QString worker_name, QString weight)
+{
+    query->prepare("select * from worker where name=:worker_name");
+    query->bindValue(":worker_name",worker_name);
+    query->exec();
+    if(!query->isActive())
+    {
+        return false;
+    }
+    int id;
+    while (query->next())
+    {
+        id = query->value(3).toInt();
+    }
+    write_time = time.currentDateTime().toString("yyyy.MM.dd hh:mm:ss");
+    query->prepare("update list set weight2=:weight,time2=:time where id=:list_id");
+    query->bindValue(":weight",weight);
+    query->bindValue(":time",write_time);
+    query->bindValue(":list_id",QString::number(id,10));
+    query->exec();
     if(!query->isActive())
     {
         return false;
@@ -134,6 +153,28 @@ bool My_Sqlite::set_flag(QString worker_name)
         {
             return false;
         }
+    }
+    return true;
+}
+
+bool My_Sqlite::get_kind(QString &kind)
+{
+    int kind_num;
+    query->exec("select * from kind_now");
+    while(query->next())
+    {
+        kind_num = query->value(1).toInt();
+    }
+    query->prepare("select * from kind where id=:kind_id");
+    query->bindValue("kind_id",QString::number(kind_num,10));
+    query->exec();
+    if(!query->isActive())
+    {
+        return false;
+    }
+    while(query->next())
+    {
+        kind = query->value(1).toString();
     }
     return true;
 }
--- my_sqlite.h
+++ my_sqlite.h
@@ -10,14 +10,14 @@ public:
     QSqlQuery *query;
     QSqlDatabase db;
     QString write_time;
-    QDateTime *time;
+    QDateTime time;
     //QString id; //list的id
     bool get_worker_name(QString card_num,QString &worker_name);
     int check_flag(QString worker_name);
     bool set_flag(QString worker_name);
-    bool check_kind(QString &kind);
     bool add_weight_1(QString worker_name,QString kind,QString weight);//新加一条，再写入毛料重量,时间
     bool add_weight_2(QString worker_name,QString weight);//成品重量加时间.id用来在list中检索
+    bool get_kind(QString &kind);
     bool add_date();
     bool test_add(QString test);
 };
